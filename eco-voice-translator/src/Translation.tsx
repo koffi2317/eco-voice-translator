a385d14ddfb942e7b7c73e6dc1e68dd9495ed428
koffi2317 <koffiakakpo23@gmail.com>
2026-02-21 15:37:29 -0500

    aout de la logique en block dur

--- eco-voice-translator/src/Translation.tsx
+++ eco-voice-translator/src/Translation.tsx
@@ -8,7 +8,42 @@ return <>
 
 <button>Commencer a enregistrer votre voix</button>
 
+<div>
+<h3>Le text original</h3>
+
+</div>
+
+<div>  
+<h3>Le text traduit</h3>
+
+
+</div>
+
+
+<button>Parler</button> 
+<button>Traduction</button>
+
+<div>
+<h3>Impact de cette traduction:</h3>
+
+</div>
+
+
+<div>
+<h3>Historique et total de la session : </h3>
+
+</div>
+
+
+<div>
+<h3>Analyse environnementales de la journée: </h3>
+
+</div>
+
+
 <button onClick={() => window.location.href = "/"}>Retour à la présentation</button>
+
+
 </>
 
 
