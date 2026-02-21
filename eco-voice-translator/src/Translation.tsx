4ad06b1e0509deac2140feb39c5df5afd09db657
koffi2317 <koffiakakpo23@gmail.com>
2026-02-21 14:41:38 -0500

    ajout de deux autres boutons pour translation.tsx

--- eco-voice-translator/src/Translation.tsx
+++ eco-voice-translator/src/Translation.tsx
@@ -6,6 +6,9 @@ return <>
 <h1>Voici la page pour la traduction IA</h1> 
 <p>Cette page affiche les résultats de la traduction IA avec l'analyse de l'empreinte carbone.</p>
 
+<button>Commencer a enregistrer votre voix</button>
+
+<button onClick={() => window.location.href = "/"}>Retour à la présentation</button>
 </>
 
 
