b1c82497cff23821434665824d12d389c41ad023
koffi2317 <koffiakakpo23@gmail.com>
2026-02-22 21:55:36 -0500

    modif

--- server/index.js
+++ server/index.js
@@ -9,6 +9,20 @@ app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-app.listen(3001, () => {
-  console.log("Server listening on port 3001");
+app.post("/api/translate", (req, res) => {
+
+     console.log("POST /api/translate called with body:", req.body);
+  const { text, source, target } = req.body;
+
+  // TODO: ici plus tard on mettra l'appel à Google Translate.
+  // Pour l’instant, on renvoie une fake traduction en anglais.
+  const fakeEnglish =
+    "This is a fake English translation of: " + text;
+
+  res.json({ translatedText: fakeEnglish });
+});
+
+
+app.listen(4000, () => {
+  console.log("Server listening on port 4000");
 });
